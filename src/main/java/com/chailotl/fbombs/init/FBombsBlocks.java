c43d46b1b9cd03f778c1c1780f6f60c2c2d23eb2
Chai <7232280+Chailotl@users.noreply.github.com>
2024-10-11 20:39:36 -0400

    Added dispenser behavior registration

--- src/main/java/com/chailotl/fbombs/init/FBombsBlocks.java
+++ src/main/java/com/chailotl/fbombs/init/FBombsBlocks.java
@@ -2,17 +2,27 @@ package com.chailotl.fbombs.init;
 
 import com.chailotl.fbombs.FBombs;
 import com.chailotl.fbombs.block.*;
+import com.chailotl.fbombs.entity.AbstractTntEntity;
 import com.chailotl.fbombs.entity.InstantTntEntity;
 import com.chailotl.fbombs.entity.LongFuseTntEntity;
 import com.chailotl.fbombs.entity.ShortFuseTntEntity;
 import com.chailotl.fbombs.util.TntEntityProvider;
 import com.chailotl.fbombs.util.TntEntityType;
 import net.minecraft.block.*;
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
+import net.minecraft.entity.TntEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPointer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,7 +68,23 @@ public class FBombsBlocks {
                 .solidBlock(Blocks::never)
         );
         TNT_BLOCKS.add(block);
-        return register(name, block, true);
+        register(name, block, true);
+        DispenserBlock.registerBehavior(block, new ItemDispenserBehavior() {
+            @Override
+            protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+                World world = pointer.world();
+                BlockPos blockPos = pointer.pos().offset(pointer.state().get(DispenserBlock.FACING));
+                AbstractTntEntity tntEntity = tntEntityProvider.spawn(world, (double)blockPos.getX() + 0.5, blockPos.getY(), (double)blockPos.getZ() + 0.5, null);
+                world.spawnEntity(tntEntity);
+                if (tntEntity.getFuse() >= 10) {
+                    world.playSound(null, tntEntity.getX(), tntEntity.getY(), tntEntity.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                }
+                world.emitGameEvent(null, GameEvent.ENTITY_PLACE, blockPos);
+                stack.decrement(1);
+                return stack;
+            }
+        });
+        return block;
     }
 
     public static void initialize() {
