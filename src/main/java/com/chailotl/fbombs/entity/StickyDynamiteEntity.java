77d893e5344511ffba7670bf1a544e1a31bbfc0c
Chai <7232280+Chailotl@users.noreply.github.com>
2024-10-29 12:44:34 -0400

    Improved sticky dynamite behavior, letting it stick to entities

--- src/main/java/com/chailotl/fbombs/entity/StickyDynamiteEntity.java
+++ src/main/java/com/chailotl/fbombs/entity/StickyDynamiteEntity.java
@@ -2,18 +2,25 @@ package com.chailotl.fbombs.entity;
 
 import com.chailotl.fbombs.init.FBombsEntityTypes;
 import com.chailotl.fbombs.init.FBombsItems;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.BlazeEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class StickyDynamiteEntity extends DynamiteEntity {
+    private boolean stuck = false;
+    private LivingEntity stuckEntity = null;
+    private Vec3d stuckOffset = Vec3d.ZERO;
+
     public StickyDynamiteEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -42,8 +49,41 @@ public class StickyDynamiteEntity extends DynamiteEntity {
     }
 
     @Override
+    public void tick() {
+        super.tick();
+
+        if (this.stuckEntity != null) {
+            if (this.stuckEntity.isAlive()) {
+                Vec3d pos = this.stuckEntity.getPos().add(this.stuckOffset);
+                this.setPos(pos.x, pos.y, pos.z);
+                this.setVelocity(Vec3d.ZERO);
+            } else {
+                this.setVelocity(this.stuckEntity.getVelocity());
+                this.stuckEntity = null;
+                this.setNoGravity(false);
+            }
+        } else if (this.stuck) {
+            this.setVelocity(Vec3d.ZERO);
+        }
+    }
+
+    @Override
     protected void onBlockHit(BlockHitResult blockHitResult) {
         super.onBlockHit(blockHitResult);
-        this.setNoGravity(true);
+        if (this.stuckEntity == null) {
+            this.stuck = true;
+            this.setNoGravity(true);
+        }
+    }
+
+    @Override
+    protected void onEntityHit(EntityHitResult entityHitResult) {
+        super.onEntityHit(entityHitResult);
+        if (this.stuckEntity == null && entityHitResult.getEntity() instanceof LivingEntity entity) {
+            this.stuckEntity = entity;
+            this.stuckOffset = this.getPos().subtract(entity.getPos()).multiply(0.5, 1, 0.5);
+            this.setNoGravity(true);
+            this.setVelocity(Vec3d.ZERO);
+        }
     }
 }
