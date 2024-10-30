82f56c7844f46f30e106e639a6c9ec302b5d482f
Chai <7232280+Chailotl@users.noreply.github.com>
2024-10-29 21:41:51 -0400

    Added loot table to gunpowder trail which doesn't work :/

--- src/main/java/com/chailotl/fbombs/datagen/BlockLootTableProvider.java
+++ src/main/java/com/chailotl/fbombs/datagen/BlockLootTableProvider.java
@@ -5,6 +5,7 @@ import com.chailotl.fbombs.block.GenericTntBlock;
 import com.chailotl.fbombs.init.FBombsBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryWrapper;
 
@@ -19,5 +20,6 @@ public class BlockLootTableProvider extends FabricBlockLootTableProvider {
     public void generate() {
         FBombs.streamEntries(Registries.BLOCK, block -> block instanceof GenericTntBlock).forEach(this::addDrop);
         addDrop(FBombsBlocks.TNT_SLAB);
+        addDrop(FBombsBlocks.GUNPOWDER_TRAIL, Items.GUNPOWDER);
     }
 }
