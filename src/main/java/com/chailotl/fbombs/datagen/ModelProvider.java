15accdb96d822419ed1a71f44cf12235e3901e19
Chai <7232280+Chailotl@users.noreply.github.com>
2024-10-29 20:59:46 -0400

    Fixed TNT slab item model

--- src/main/java/com/chailotl/fbombs/datagen/ModelProvider.java
+++ src/main/java/com/chailotl/fbombs/datagen/ModelProvider.java
@@ -66,8 +66,9 @@ public class ModelProvider extends FabricModelProvider {
             blockStateModelGenerator.registerSingleton(block, TexturedModel.CUBE_BOTTOM_TOP);
         });*/
 
-        blockStateModelGenerator.blockStateCollector.accept(VariantsBlockStateSupplier.create(FBombsBlocks.SPLIT_TNT)
-            .coordinate(createSplitTntBlockState()));
+        blockStateModelGenerator.blockStateCollector.accept(
+            VariantsBlockStateSupplier.create(FBombsBlocks.SPLIT_TNT).coordinate(createSplitTntBlockState())
+        );
         blockStateModelGenerator.blockStateCollector.accept(
             BlockStateModelGenerator.createSlabBlockState(FBombsBlocks.TNT_SLAB,
                 FBombs.getId("block/tnt_slab_bottom"),
@@ -75,9 +76,7 @@ public class ModelProvider extends FabricModelProvider {
                 FBombs.getId("block/tnt_slab_double")
             )
         );
-
-        //TODO: [ShiroJR] apply proper block item model
-        blockStateModelGenerator.registerParentedItemModel(FBombsBlocks.TNT_SLAB.asItem(), FBombs.getId("block/tnt_slab_bottom"));
+        blockStateModelGenerator.registerParentedItemModel(FBombsBlocks.TNT_SLAB, FBombs.getId("block/tnt_slab_bottom"));
     }
 
     @Override
