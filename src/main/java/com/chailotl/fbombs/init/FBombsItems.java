bc0a1a0a5967f0a992b459afbfd2a24e87b442ed
JR1811 <janreichelt77@gmail.com>
2024-10-30 23:04:15 +0100

    fixed problematic logical statement in Siren PosWalker and small refactor

--- src/main/java/com/chailotl/fbombs/init/FBombsItems.java
+++ src/main/java/com/chailotl/fbombs/init/FBombsItems.java
@@ -11,13 +11,7 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
-@SuppressWarnings("unused")
 public class FBombsItems {
-    private static final List<Item> VALUES = new ArrayList<>();
 
     public static final DynamiteItem DYNAMITE = register("dynamite", new DynamiteItem(DynamiteEntity::new, new Item.Settings()), FBombsItemGroups.ITEMS);
     public static final DynamiteItem BOUNCY_DYNAMITE = register("bouncy_dynamite", new DynamiteItem(BouncyDynamiteEntity::new, new Item.Settings()), FBombsItemGroups.ITEMS);
@@ -25,9 +19,7 @@ public class FBombsItems {
     public static final DynamiteItem DYNAMITE_BUNDLE = register("dynamite_bundle", new DynamiteItem(DynamiteBundleEntity::new, new Item.Settings()), FBombsItemGroups.ITEMS);
     public static final BlockItem GUNPOWDER_TRAIL = register("gunpowder_trail", new BlockItem(FBombsBlocks.GUNPOWDER_TRAIL, new Item.Settings()));
 
-    @SuppressWarnings("SameParameterValue")
     private static <T extends Item> T register(String name, T item, FBombsItemGroups.ItemGroupEntry... itemGroups) {
-        VALUES.add(item);
         Registry.register(Registries.ITEM, FBombs.getId(name), item);
         if (itemGroups != null) {
             for (var entry : itemGroups) {
@@ -39,9 +31,5 @@ public class FBombsItems {
 
     public static void initialize() {
         // static initialisation
-    }
-
-    public static Stream<Item> stream() {
-        return VALUES.stream();
     }
 }
